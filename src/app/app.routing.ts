91f9f46d9dbc5fb7e84c438451992d71d80999a6
Estela Romero Muñoz <estelaromer@gmail.com>
2018-06-01 05:45:13 +0200

    Botón de Iniciar sesión del componente loginarea te lleva al componente Login

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,3 +1,10 @@
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-export const appRoutes: Routes = [];
+export const appRoutes: Routes = [
+    { path: '', component: HomepageComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'registro', component: SignupComponent}
+];
--- src/app/loginarea/loginarea.component.ts
+++ src/app/loginarea/loginarea.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loginarea',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginareaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+  handleLoginArea() {
+    this.router.navigate(['login']);
+  }
   ngOnInit() {
   }
 
