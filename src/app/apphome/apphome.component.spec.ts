ecac82fa80d80c00c76ea6541c11040277136215
Estela Romero Muñoz <estelaromer@gmail.com>
2018-06-02 11:49:10 +0200

    en el app home tendremos la vista general de la app

---
+++ src/app/apphome/apphome.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ApphomeComponent } from './apphome.component';
+
+describe('ApphomeComponent', () => {
+  let component: ApphomeComponent;
+  let fixture: ComponentFixture<ApphomeComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ApphomeComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ApphomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
